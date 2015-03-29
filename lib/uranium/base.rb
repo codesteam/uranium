7262b45d3434077ffc4b21e1d1e72e1d6bcb0887
Ryabov Ruslan <diserve.it@gmail.com>
2015-03-30 01:55:17 +0300

    method for render only api doc html

--- lib/uranium/base.rb
+++ lib/uranium/base.rb
@@ -8,23 +8,29 @@ module Uranium
   class Core
     def initialize(source, options = {})
       @options = {
-        :view_layout   => File.expand_path(File.dirname(__FILE__))+'/templates/layout.html.erb',
-        :view_api_path => File.expand_path(File.dirname(__FILE__))+'/templates/_api_path.html.erb',
+        :view_layout    => File.expand_path(File.dirname(__FILE__))+'/templates/layout.html.erb',
+        :view_api_path  => File.expand_path(File.dirname(__FILE__))+'/templates/_api_path.html.erb',
+        :view_container => File.expand_path(File.dirname(__FILE__))+'/templates/_container.html.erb',
       }
       @options = @options.merge(options)
       @source  = source
     end
 
-    def decay
+    def decay(use_layout = true)
       @document       = YAML::parse(@source).root.to_ruby
       @document_info  = ParserApiInfo.new(@document)
       @document_paths = ParserApiPaths.new(@document)
 
-      ERB.new(File.open(@options[:view_layout]){|file| file.read}).result(binding)
+      return ERB.new(File.open(@options[:view_layout]){|file| file.read}).result(binding) if use_layout
+      return render_container() if !use_layout
     end
 
     def render_api_method(method)
       ERB.new(File.open(@options[:view_api_path]){|file| file.read}).result(method.instance_eval { binding })
+    end
+
+    def render_container()
+      ERB.new(File.open(@options[:view_container]){|file| file.read}).result(binding)
     end
   end
 end
