eb1e27f1e181edbb2cf3e5869e3ff86af3702b3a
Ryabov Ruslan <diserve.it@gmail.com>
2015-05-04 00:04:43 +0300

    render response

--- lib/uranium/parsers/parser_api_path.rb
+++ lib/uranium/parsers/parser_api_path.rb
@@ -1,7 +1,7 @@
 module Uranium
   module Parsers
     class ApiPath
-      attr_reader :header, :querytype, :summary, :description, :parameters
+      attr_reader :header, :querytype, :description, :parameters, :responses
 
       def initialize(path, definitions_parser)
         @querytype_values  = ['GET', 'POST', 'PUT', 'DELETE']
@@ -11,14 +11,12 @@ module Uranium
         raise 'Path header section not specified'      if path[0].nil?
         raise 'Path information not specified'         if path[1].nil?
         raise 'Path query type section not specified'  if path[1]['query'].nil?
-        raise 'Path summary section not specified'     if path[1]['summary'].nil?
         raise 'Path description section not specified' if path[1]['description'].nil?
         raise 'Query parameters not specified'         if path[1]['parameters'].nil?
         raise 'Path responses section not specified'   if path[1]['responses'].nil?
 
         @header      = path[0]
         @querytype   = path[1]['query']
-        @summary     = path[1]['summary']
         @description = path[1]['description']
         @parameters  = path[1]['parameters']
         @responses   = path[1]['responses']
